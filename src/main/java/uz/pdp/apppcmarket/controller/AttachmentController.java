ecea55da8aa5ff25bb45e11946af818fef81ec5e
Husanboy <husanboysobirjonov02@gmail.com>
2022-01-08 03:02:52 +0500

    security implemented

--- src/main/java/uz/pdp/apppcmarket/controller/AttachmentController.java
+++ src/main/java/uz/pdp/apppcmarket/controller/AttachmentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -25,8 +26,10 @@ import java.util.Optional;
 @RequestMapping(value = "/api/attachment")
 public class AttachmentController {
 
+
     @Autowired
     AttachmentDownloadService attachmentDownloadService;
+
 
     @GetMapping(value = "/download/{id}")
     public void downloadAttachment(@PathVariable Integer id, HttpServletResponse httpServletResponse)  {
--- src/main/java/uz/pdp/apppcmarket/controller/ProductController.java
+++ src/main/java/uz/pdp/apppcmarket/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.apppcmarket.dto.ObjectResponse;
 import uz.pdp.apppcmarket.dto.ProductDto;
@@ -20,24 +21,28 @@ public class ProductController {
 
 
     @GetMapping
+    @PreAuthorize(value = "hasAnyRole('SUPER_ADMIN','MODERATOR','OPERATOR')")
     public HttpEntity<?> getAll(@RequestParam(defaultValue = "20") Integer size, @RequestParam(defaultValue = "0") Integer page){
         Page<Product> productPage =  productService.getAllService(size,page);
         return ResponseEntity.ok(productPage);
     }
 
     @PostMapping
+    @PreAuthorize(value = "hasAnyRole('MODERATOR','SUPER_ADMIN')")
     public HttpEntity<?> add(@RequestBody ProductDto productDto){
         Response response = productService.addProduct(productDto);
         return ResponseEntity.status(response.getSuccess()? HttpStatus.OK:HttpStatus.CONFLICT).body(response);
     }
 
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize(value = "hasRole('SUPER_ADMIN')")
     public HttpEntity<?> delete(@PathVariable Integer id){
         Response response = productService.deleteById(id);
         return ResponseEntity.status(response.getSuccess()?HttpStatus.NO_CONTENT:HttpStatus.NOT_FOUND).body(response);
     }
 
     @PutMapping(value = "/{id}")
+    @PreAuthorize(value = "hasAnyRole('SUPER_ADMIN','MODERATOR')")
     public HttpEntity<?> edit(@PathVariable Integer id,@RequestBody ProductDto productDto){
         ObjectResponse objectResponse = productService.editProduct(id, productDto);
         return ResponseEntity.status(objectResponse.getResponse().getSuccess()?HttpStatus.OK:HttpStatus.CONFLICT).body(objectResponse);
